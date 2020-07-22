8fc955a11bdce5e828f0b3fa8219018c31d194f1
nikita.dotsenko <indahazzard@gmail.com>
2020-07-22 22:37:48 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper" v-for="(image, key) in imagesCollection" :key="key" :class="{'active': key===currentImageIndex && currentImageIndex!==0}">
+      <div class="background" :style="{'--bkgImage': 'url(' + image + ')', '--bkgImageMobile': 'url(' + image + ')'}"></div>
+      <div class="img-content">
+        <img :src="image">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {},
+    props: {
+      interval: {
+        type: Number,
+        default: 2000
+      },
+      animationStyle: {
+        type: String,
+        default: 'box'
+      },
+      animationCurve: {
+        type:String,
+        default: 'linear'
+      },
+      animationLoop: {
+        type: String,
+        default: 'infinite'
+      }
+    },
+    data () {
+      return {
+        currentImageIndex:0
+      }
+    },
+    computed: {
+      imagesCollection() {
+        return [
+          'covers/xx.png',
+          'covers/astroworld.jpeg',
+          'covers/discogs.jpeg',
+          'covers/syre.jpeg',
+          'covers/mars.jpeg',
+          'covers/xx.png',
+        ];
+      },
+    },
+    mounted() {
+     setInterval(() => this.swapImage(), this.interval)
+    },
+    methods: {
+      swapImage () {
+        this.currentImageIndex += 1
+        if (this.currentImageIndex === this.imagesCollection.length){
+          this.currentImageIndex = 1
+        }
+      }
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .wrapper {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    left: 0;
+  }
+  .active {
+    z-index: 4;
+    animation: box 2s infinite linear;
+    position: relative;
+  }
+  .background {
+    background-image: var(--bkgImage);
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter: blur(1.5rem);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+  }
+
+  .img-content {
+    position: relative;
+    width: 400px;
+    height: 400px;
+    z-index: 1;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  @keyframes box {
+    from { z-index: 2; opacity: 0.1; }
+    to { z-index: 4; opacity: 1; }
+  }
 </style>
